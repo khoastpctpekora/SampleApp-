7db72c9c2b0510701ca5859fd63ba655efaff8ad
NguyenNhoAnhKhoa <105312661@student.swin.edu.au>
2026-08-18 13:42:43 +1000

    Add files via upload

---
+++ Greeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TextLib
+{
+    // This class is compiled into TextLib.dll and used by SampleApp.exe
+    public class Greeter
+    {
+        public string Greet(string name)
+        {
+            return "TextLib: Hello, " + name + "! Welcome to Software Deployment.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using MathLib;
+using Newtonsoft.Json;
+using System;
+using TextLib;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SWE40006 - Deployment Portfolio Task 1");
+            Console.WriteLine("Nho Anh Khoa Nguyen - 105312661");
+            Console.WriteLine("--------------------------------------");
+
+            // Dependency 1: MathLib.dll (custom class library)
+            var calc = new Calculator();
+            Console.WriteLine("MathLib:  12 + 30 = " + calc.Add(12, 30));
+            Console.WriteLine("MathLib:  6 x 7   = " + calc.Multiply(6, 7));
+
+            // Dependency 2: TextLib.dll (custom class library)
+            var greeter = new Greeter();
+            Console.WriteLine(greeter.Greet("Khoa"));
+
+            // Dependency 3: Newtonsoft.Json.dll (third-party NuGet DLL)
+            var info = new
+            {
+                App = "SampleApp",
+                Version = "1.0.0",
+                Dependencies = new[] { "MathLib.dll", "TextLib.dll", "Newtonsoft.Json.dll" }
+            };
+            string json = JsonConvert.SerializeObject(info, Formatting.Indented);
+            Console.WriteLine("Newtonsoft.Json output:");
+            Console.WriteLine(json);
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Deployment Task completed! Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
