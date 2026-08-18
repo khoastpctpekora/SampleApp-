68d5e3d3896e0328fdbb8cf9a7310a1fc9ae777b
NguyenNhoAnhKhoa <105312661@student.swin.edu.au>
2026-08-18 13:35:39 +1000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathLib
+{
+    // This class is compiled into MathLib.dll and used by SampleApp.exe
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
